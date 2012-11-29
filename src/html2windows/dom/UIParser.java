25e8c415ff5e3abe8a79681e168316954d7d63b1
bee040811 <bee040811@bee040811-Aspire-5610>
2012-11-30 01:21:29 +0800

    add UIparser test

--- src/html2windows/dom/UIParser.java
+++ src/html2windows/dom/UIParser.java
@@ -1,16 +1,55 @@
 package html2windows.dom;
 import java.io.File;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class UIParser {
     public Document createDocument(){
         return null;
     }
     
     public Document parse(String input){
-        return null;
+    	try {
+
+    			File stocks = new File("Stocks.xml");
+	    		
+    			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	    		Document doc = (Document) dBuilder.parse(stocks);
+	    		//((org.w3c.dom.Node) doc.documentElement()).normalize();
+/*
+	    		System.out.println("root of xml file" + doc.documentElement().getName());
+	    		NodeList nodes = doc.getElementsByTagName("stock");
+	    		System.out.println("==========================");
+
+	    		for (int i = 0; i < nodes.length(); i++) {
+		    		Node node = nodes.item(i);
+		
+		    		if (node.nodeType() == Node.ELEMENT_NODE) {
+			    		Element element = (Element) node;
+			    		System.out.println("Stock Symbol: " + getValue("symbol", element));
+			    		System.out.println("Stock Price: " + getValue("price", element));
+			    		System.out.println("Stock Quantity: " + getValue("quantity", element));
+		    		}
+		    	}
+	*/	    	
+		    } catch (Exception ex) {
+		    	ex.printStackTrace();
+		    }
+	    return null;
     }
     
     public Document parse(File input){
         return null;
     }
+    private static String getValue(String tag, Element element) {
+    	NodeList nodes = element.getElementsByTagName(tag).item(0).childNodes();
+    	Node node = (Node) nodes.item(0);
+    	return node.nodeValue();
+    }
+    
+
+
+    	
 }
