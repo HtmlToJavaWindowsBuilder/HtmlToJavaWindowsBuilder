43539b5b430fc1908eef883355a41b23c5261eed
cfwei <s124210@gmail.com>
2012-12-05 15:06:26 +0800

    add .classpath modify .project and Implement some method in Text.java

--- src/html2windows/dom/Text.java
+++ src/html2windows/dom/Text.java
@@ -1,105 +1,142 @@
 package html2windows.dom;
+import javax.swing.JComponent;
+
 import org.w3c.dom.DOMException;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventException;
 import org.w3c.dom.events.EventListener;
 
 
-public class Text implements Node {
+public class Text extends JComponent implements Node {
 	
-	 public String data;
-	
+	NodeList childNodeList=new NodeList();
+	private String dataValue;
 	
 	public Text(String data){
-		this.data=data;
+		dataValue=data;
 	}
 	
+	public String data(){
+		
+		return dataValue;
+	}
     public Text splitText(long offset) throws DOMException{
         return null;
     }
     
     public void setData(String data){
-        this.data=data;
+    	dataValue=data;
     }
 
     public long length(){
-        return data.length();
+        return dataValue.length();
     }
     
     public String substringData(long offset, long count) throws DOMException{
-        return data.substring((int)offset,(int)offset+(int)count);
+        return dataValue.substring((int)offset,(int)offset+(int)count);
     }
     
     public void appendData(String arg) throws DOMException{
-    	data.concat("arg");
+    	dataValue.concat("arg");
     }
     
     public void insertData(long offset, String arg) throws DOMException{
     	String newData=null;
-    	newData+=data.substring(0, (int)offset);
+    	newData+=dataValue.substring(0, (int)offset);
     	newData+=arg;
-    	newData+=data.substring((int)offset+1);
-    	
+    	newData+=dataValue.substring((int)offset+1);
+    	dataValue=newData;
     }
     
     public void deleteData(long offset, long count) throws DOMException{
+    	String newData=null;
+    	newData.concat(dataValue.substring(0,(int)offset));
+    	newData.concat(dataValue.substring((int)offset+(int)count));
+    	dataValue=newData;
     }
 
     public void replaceData(long offset, long count, String arg) throws DOMException{
+    	String newData=null;
+    	newData.concat(dataValue.substring(0,(int)offset));
+    	newData.concat(arg);
+    	newData.concat(dataValue.substring((int)offset+(int)count));
+    	dataValue=newData;
     }
 
     @Override
     public String nodeName() {
         // TODO Auto-generated method stub
-        return null;
+        return "#text";
     }
 
     @Override
     public String nodeValue() {
         // TODO Auto-generated method stub
-        return null;
+        return dataValue;
     }
 
     @Override
     public short nodeType() {
         // TODO Auto-generated method stub
-        return 0;
+        return TEXT_NODE;
     }
 
     @Override
     public Node parentNode() {
         // TODO Auto-generated method stub
-        return null;
+        return (Node)getParent();
     }
 
     @Override
     public NodeList childNodes() {
         // TODO Auto-generated method stub
-        return null;
+        return childNodeList;
     }
 
     @Override
     public Node firstChild() {
         // TODO Auto-generated method stub
-        return null;
+    	if(childNodeList.size()>0)
+    		return childNodeList.item(0);
+    	else
+    		return null;
     }
 
     @Override
     public Node lastChild() {
         // TODO Auto-generated method stub
-        return null;
+    	int nodeListSize=childNodeList.size();
+    	if(nodeListSize>0)
+    		return childNodeList.item(nodeListSize-1);
+    	else
+    		return null;
     }
 
     @Override
     public Node previousSibling() {
         // TODO Auto-generated method stub
-        return null;
+    	NodeList siblingList=((Node)getParent()).childNodes();
+    	int index=siblingList.indexOf(this);
+    	if(index==0){
+    		return null;
+    	}
+    	else{
+    		return siblingList.item(index-1);
+    	}
+
     }
 
     @Override
     public Node nextSibling() {
         // TODO Auto-generated method stub
-        return null;
+    	NodeList siblingList=((Node)getParent()).childNodes();
+    	int index=siblingList.indexOf(this);
+    	if(index==siblingList.size()-1){
+    		return null;
+    	}
+    	else{
+    		return siblingList.item(index+1);
+    	}
     }
 
     @Override
@@ -117,25 +154,53 @@ public class Text implements Node {
     @Override
     public Node insertBefore(Node newChild, Node refChild) throws DOMException {
         // TODO Auto-generated method stub
-        return null;
+    	if(newChild==null)
+    		return null;
+    	else if(refChild==null){
+    		childNodeList.add(newChild);
+    		return newChild;
+    	}
+    	else{
+    		int index=childNodeList.indexOf(refChild);
+    		childNodeList.add(index-1, newChild);
+    		return newChild;
+    	}
+        
     }
 
     @Override
     public Node replaceChild(Node newChilde, Node oldChild) throws DOMException {
         // TODO Auto-generated method stub
-        return null;
+    	int index=childNodeList.indexOf(oldChild);
+    	if(index==-1){
+    		return null;
+    	}
+    	else{
+    		childNodeList.remove(index);
+    		childNodeList.add(index, newChilde);
+    		return oldChild;
+    	}
     }
 
     @Override
     public Node removeChild(Node oldChild) throws DOMException {
         // TODO Auto-generated method stub
-        return null;
+    	int index=childNodeList.indexOf(oldChild);
+    	if(index==-1)
+    		return null;
+    	else{
+    		childNodeList.remove(index);
+    		return oldChild;
+    	}
+        
     }
 
     @Override
     public Node appendChild(Node newChild) throws DOMException {
         // TODO Auto-generated method stub
-        return null;
+    	childNodeList.add(newChild);
+        return newChild;
+        
     }
 
     @Override
@@ -147,7 +212,11 @@ public class Text implements Node {
     @Override
     public boolean hasAttributes() {
         // TODO Auto-generated method stub
-        return false;
+    	int size=childNodeList.size();
+    	if(size>0)
+    		return true;
+    	else
+    		return false;
     }
 
     @Override
