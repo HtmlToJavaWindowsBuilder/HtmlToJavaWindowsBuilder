c7f2a6698a542f5d2e484b5973d406428ea5f3ae
cfwei <s124210@gmail.com>
2012-12-06 10:43:09 +0800

    add comment , fix hasChildNodes() and hasAttributes()

--- src/html2windows/dom/Text.java
+++ src/html2windows/dom/Text.java
@@ -12,50 +12,97 @@ public class Text extends JComponent implements Node {
 	NodeList childNodeList=new NodeList();
 	private String dataValue;
 	
+	// 目的:constructor,設定此Text的dataValue 
 	public Text(String data){
 		dataValue=data;
 	}
 	
+	//目的:回傳此Text的dataValue
 	public String data(){
-		
 		return dataValue;
 	}
+	
+	/*目的:將TextData以offset為分界,切割成兩個新的Text並將其接回parent的ChildNodeList,使其二成為sibling
+	 *方法:
+	 *1.先檢查offset是否 大於零 或 在dataValue範圍內
+	 *  若否則throw DOMException
+	 *2.New兩個Text newText1及newText2
+	 *3.以offset為分界將Text切割並存入兩個新的Text裡面
+	 *4.找出parent的ChildNodeList先將舊的Text移除 最後加入兩個新的Text
+	 */
     public Text splitText(long offset) throws DOMException{
+    	if(offset<0||offset>=dataValue.length())
+    		throw new DOMException(DOMException.INDEX_SIZE_ERR	,"The specified offset is negative or greater than the number of 16-bit units in data");
+    	
+    	String newString1=dataValue.substring(0, (int)offset);
+    	String newString2=dataValue.substring((int)offset+1);
+    	Text newText1=new Text(newString1);
+    	Text newText2=new Text(newString2);
+    	
+    	NodeList parentsChildNodeList=((Node)getParent()).childNodes();
+    	int index=parentsChildNodeList.indexOf(this);
+    	parentsChildNodeList.remove(index);
+    	parentsChildNodeList.add(index,newText2);
+    	parentsChildNodeList.add(index,newText1);
+    	
         return null;
     }
     
+    //目的:將此Text的dataValue設置為參數的data
     public void setData(String data){
     	dataValue=data;
     }
 
+    //目的:回傳此Text的dataValue的長度 
     public long length(){
         return dataValue.length();
     }
     
+    /*目的:擷取此dataValue的部份片段,傳入兩個參數:
+     * offset:以此為start position
+     * count:要擷取的數目
+     * 回傳即為以start position為基準,往後算count個長度的字串
+    */
     public String substringData(long offset, long count) throws DOMException{
         return dataValue.substring((int)offset,(int)offset+(int)count);
     }
     
+    //目的:將參數字串arg串接到dataValue後
     public void appendData(String arg) throws DOMException{
     	dataValue.concat("arg");
     }
     
+    //目的:將參數字串arg串接到dataValue中的offset處
     public void insertData(long offset, String arg) throws DOMException{
     	String newData=null;
-    	newData+=dataValue.substring(0, (int)offset);
-    	newData+=arg;
-    	newData+=dataValue.substring((int)offset+1);
+    	newData.concat(dataValue.substring(0, (int)offset));
+    	newData.concat(arg);
+    	newData.concat(dataValue.substring((int)offset+1));
     	dataValue=newData;
     }
     
+    /*目的:刪除dataValue裡面部份內容
+     * offset:以此為start position
+     * count:要刪除的數目
+     * 結果即為以offset為基準,往後刪除長度為count的內容
+     * */
     public void deleteData(long offset, long count) throws DOMException{
+    	if(offset<0||count<0||offset>=dataValue.length())
+    		throw new DOMException(DOMException.INDEX_SIZE_ERR	, " The specified offset is negative or greater than the number of 16-bit units in data, or if the specified count is negative");
     	String newData=null;
     	newData.concat(dataValue.substring(0,(int)offset));
     	newData.concat(dataValue.substring((int)offset+(int)count));
     	dataValue=newData;
     }
 
+    
+    /*目的:以新字串arg取代dataValue中以offset為基準長度為count的部份
+     * 
+     * */
     public void replaceData(long offset, long count, String arg) throws DOMException{
+    	if(offset<0||count<0||offset>=dataValue.length())
+    		throw new DOMException(DOMException.INDEX_SIZE_ERR	, " The specified offset is negative or greater than the number of 16-bit units in data, or if the specified count is negative");
+    	
     	String newData=null;
     	newData.concat(dataValue.substring(0,(int)offset));
     	newData.concat(arg);
@@ -63,36 +110,42 @@ public class Text extends JComponent implements Node {
     	dataValue=newData;
     }
 
+    //目的:回傳nodeName
     @Override
     public String nodeName() {
         // TODO Auto-generated method stub
         return "#text";
     }
-
+    
+    //目的:回傳nodeValue 
     @Override
     public String nodeValue() {
         // TODO Auto-generated method stub
         return dataValue;
     }
-
+    
+    //目的:回傳nodeType
     @Override
     public short nodeType() {
         // TODO Auto-generated method stub
         return TEXT_NODE;
     }
 
+    //目的:回傳parentNode
     @Override
     public Node parentNode() {
         // TODO Auto-generated method stub
         return (Node)getParent();
     }
-
+    
+    //目的:回傳childNodeList
     @Override
     public NodeList childNodes() {
         // TODO Auto-generated method stub
         return childNodeList;
     }
 
+    //目的:回傳childNodeList中第一個child
     @Override
     public Node firstChild() {
         // TODO Auto-generated method stub
@@ -102,6 +155,7 @@ public class Text extends JComponent implements Node {
     		return null;
     }
 
+    //目的:回傳childNodeList中最後一個child
     @Override
     public Node lastChild() {
         // TODO Auto-generated method stub
@@ -111,7 +165,8 @@ public class Text extends JComponent implements Node {
     	else
     		return null;
     }
-
+    
+    //目的:回傳前一個sibling
     @Override
     public Node previousSibling() {
         // TODO Auto-generated method stub
@@ -125,7 +180,7 @@ public class Text extends JComponent implements Node {
     	}
 
     }
-
+    //目的:回傳後一個sibling
     @Override
     public Node nextSibling() {
         // TODO Auto-generated method stub
@@ -150,7 +205,8 @@ public class Text extends JComponent implements Node {
         // TODO Auto-generated method stub
         return null;
     }
-
+    
+    //目的:將newChild insert到refChild之前
     @Override
     public Node insertBefore(Node newChild, Node refChild) throws DOMException {
         // TODO Auto-generated method stub
@@ -167,7 +223,7 @@ public class Text extends JComponent implements Node {
     	}
         
     }
-
+    //目的:用newChilde取代oldChild
     @Override
     public Node replaceChild(Node newChilde, Node oldChild) throws DOMException {
         // TODO Auto-generated method stub
@@ -181,7 +237,8 @@ public class Text extends JComponent implements Node {
     		return oldChild;
     	}
     }
-
+    
+    //目的:移除oldChild
     @Override
     public Node removeChild(Node oldChild) throws DOMException {
         // TODO Auto-generated method stub
@@ -194,29 +251,32 @@ public class Text extends JComponent implements Node {
     	}
         
     }
-
+    
+    //目的:將newChild append到childNodeList
     @Override
     public Node appendChild(Node newChild) throws DOMException {
         // TODO Auto-generated method stub
     	childNodeList.add(newChild);
         return newChild;
-        
     }
-
+    
+    //目的:回傳看此Node是否有childNode
     @Override
     public boolean hasChildNodes() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    @Override
-    public boolean hasAttributes() {
         // TODO Auto-generated method stub
     	int size=childNodeList.size();
     	if(size>0)
     		return true;
     	else
     		return false;
+        
+    }
+
+    //目的:回傳此點是否有Attribute：
+    @Override
+    public boolean hasAttributes() {
+        // TODO Auto-generated method stub
+    	return false;
     }
 
     @Override
