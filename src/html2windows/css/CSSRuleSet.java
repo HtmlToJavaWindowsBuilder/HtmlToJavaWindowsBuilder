eb376ea362e0c4e33c16329b9d5e64c37edeaa98
jason <jason_920248@hotmail.com>
2012-12-11 15:13:43 +0800

    12/11

--- src/html2windows/css/CSSRuleSet.java
+++ src/html2windows/css/CSSRuleSet.java
@@ -1,7 +1,17 @@
 package html2windows.css;
 
+import java.util.HashMap;
+
 public class CSSRuleSet {
+	HashMap<String,String> cssRule=new HashMap<String,String>();
+	public int priority;
+	public CSSRuleSet(int priority){
+		this.priority=priority;
+	}
     public String getProperty(String propertyName){
-        return propertyName;
+        return cssRule.get(propertyName);
+    }
+    public void setProperty(String propertyName, String value){
+    	cssRule.put(propertyName,value);
     }
 }
--- src/html2windows/css/Style.java
+++ src/html2windows/css/Style.java
@@ -1,25 +1,77 @@
 package html2windows.css;
 
+import java.util.ArrayList;
+
 import html2windows.dom.Element;
 
 public class Style {
+	Element element;
+	ArrayList<CSSRuleSet> ruleSetList=new ArrayList<CSSRuleSet>();
+	
+	/*
+	 * 建構子
+	 * 建立style並加入inline property的CSSRuleSet
+	 * arg1 parent node
+	 */
+	public Style(Element element){
+		this.element=element;
+		CSSRuleSet ruleSet=new CSSRuleSet(5);
+		ruleSetList.add(ruleSet);
+	}
+	
+	/*
+	 * 設css rule set的property
+	 * inline設定property 
+	 * arg1 priority名稱 
+	 * arg2 priority value
+	 * arg3 CSSRuleSet的priority
+	 */
     public void setProperty(String propertyName, String value){
-        
+        ruleSetList.get(0).setProperty(propertyName,value);
     }
     
+    /*
+     * 取得property
+     * 依照arrayList的順序(property)取得property的值
+     * arg1 property的名稱
+     * return property value
+     */
     public String getProperty(String propertyName){
-        return propertyName;
-    }
-  
-    public void addCSSRuleSet(CSSRuleSet cssRuleSet){
-        
-    }
-    
-    public Element getElement(){
+    	
+    	String value=null;
+    	for(CSSRuleSet ruleSet : ruleSetList){
+    		value=ruleSet.getProperty(propertyName);
+    		if(value!=null)
+    			return value;
+    	}
+    	//找不到property value
         return null;
     }
+  
+    /*
+     * 加入Css ruleset
+     * 把新的ruleset 加入css ruleset的arraylist
+     * arg1 ruleset
+     * arg2 ruleset的priority
+     */
+    public void addCSSRuleSet(CSSRuleSet cssRuleSet,int priority){
+    	//arrayList由大到小排序
+    	//利用priority大小決定要插入到哪裡
+    	for(int i=0;i<ruleSetList.size();i++){
+    		if(ruleSetList.get(i).priority<=cssRuleSet.priority){
+    			ruleSetList.add(i,cssRuleSet);
+    			return;
+    		}
+    	}
+    	//當空的或是priority最小的時候加入
+    	ruleSetList.add(cssRuleSet);
+    }
     
-    public void clearRules(){
-        
+    /*
+     * 取得parent node
+     * return prarent node
+     */
+    public Element getElement(){
+        return element;
     }
 }
