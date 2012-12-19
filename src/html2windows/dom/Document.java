5c008c14efd86a46e6c6bbab495aae16610a0e72
southernbear <xq5530303@gmail.com>
2012-12-19 16:16:09 +0800

    Indent

--- src/html2windows/dom/Document.java
+++ src/html2windows/dom/Document.java
@@ -21,157 +21,157 @@ import org.w3c.dom.events.EventListener;
 @SuppressWarnings("serial")
 
 public class Document extends JFrame implements Node{
-    public Element documentElement(){
-        try{
+	public Element documentElement(){
+		try{
 			return (Element)getComponent(0);
 		}
 		catch(Exception ex){
 			return null;
 		}
-    }
-    
-    public Element createElement(String tagName) throws DOMException{
-    	ElementInter element = new ElementInter(tagName);
-    	element.setOwnerDocument(this);
-        return element;
-    }
-    
-    public DocumentFragment createDocumentFragment(){
-        DocumentFragmentInter documentFragement = new DocumentFragmentInter();
-        documentFragement.setOwnerDocument(this);
-        return documentFragement;
-    }
-    
-    public Text createTextNode(String data){
-        TextInter text = new TextInter(data);
-        text.setOwnerDocument(this);
-        return text;
-    }
-    
-    public Attr createAttribute(String name) throws DOMException{
-        AttrInter attr = new AttrInter(name);
-        attr.setOwnerDocument(this);
-        return attr;
-    }
-    
-    /**
-     * Get list of elements which have specific tag name.
-     *
-     * Now we do not support live NodeList but a static NodeList so you need to
-     * call this function right before you use it to get the correct snapshot.
-     */
-    public NodeList getElementsByTagName(String tagname){
-    	if(documentElement() != null)
+	}
+	
+	public Element createElement(String tagName) throws DOMException{
+		ElementInter element = new ElementInter(tagName);
+		element.setOwnerDocument(this);
+		return element;
+	}
+	
+	public DocumentFragment createDocumentFragment(){
+		DocumentFragmentInter documentFragement = new DocumentFragmentInter();
+		documentFragement.setOwnerDocument(this);
+		return documentFragement;
+	}
+	
+	public Text createTextNode(String data){
+		TextInter text = new TextInter(data);
+		text.setOwnerDocument(this);
+		return text;
+	}
+	
+	public Attr createAttribute(String name) throws DOMException{
+		AttrInter attr = new AttrInter(name);
+		attr.setOwnerDocument(this);
+		return attr;
+	}
+	
+	/**
+	 * Get list of elements which have specific tag name.
+	 *
+	 * Now we do not support live NodeList but a static NodeList so you need to
+	 * call this function right before you use it to get the correct snapshot.
+	 */
+	public NodeList getElementsByTagName(String tagname){
+		if(documentElement() != null)
 			return documentElement().getElementsByTagName(tagname);
 		else
-        	return new NodeList();
-    }
-    
-    public Node importNode(Node importedNode, boolean deep) throws DOMException{
-    	NodeInter newNode = null;
-    
-    	switch(importedNode.nodeType()){
-    	case ATTRIBUTE_NODE :
-    		newNode = importNode((Attr)importedNode, deep);
-    		break;
-    		
-    	case DOCUMENT_FRAGMENT_NODE :
-    		newNode = importNode((DocumentFragment)importedNode, deep);
-    		break;
-    		
-    	case ELEMENT_NODE :
-    		newNode = importNode((Element)importedNode, deep);
-    		break;
-    		
-    	case TEXT_NODE :
-    		newNode = importNode((Text)importedNode, deep);
-    		break;
-    		
+			return new NodeList();
+	}
+	
+	public Node importNode(Node importedNode, boolean deep) throws DOMException{
+		NodeInter newNode = null;
+	
+		switch(importedNode.nodeType()){
+		case ATTRIBUTE_NODE :
+			newNode = importNode((Attr)importedNode, deep);
+			break;
+			
+		case DOCUMENT_FRAGMENT_NODE :
+			newNode = importNode((DocumentFragment)importedNode, deep);
+			break;
+			
+		case ELEMENT_NODE :
+			newNode = importNode((Element)importedNode, deep);
+			break;
+			
+		case TEXT_NODE :
+			newNode = importNode((Text)importedNode, deep);
+			break;
+			
 		default :
 			throw new DOMException(DOMException.NOT_SUPPORTED_ERR, "The type of node cannot be imported");
-    	}
-    	
-    	newNode.setOwnerDocument(this);
-    	
-    	return newNode;
-    }
-    
-    public Element getElementById(String elementId){
-        return getElementByIdInternal(documentElement(), elementId);
-    }
+		}
+		
+		newNode.setOwnerDocument(this);
+		
+		return newNode;
+	}
+	
+	public Element getElementById(String elementId){
+		return getElementByIdInternal(documentElement(), elementId);
+	}
 
-    @Override
-    public String nodeName() {
-        return "#document";
-    }
+	@Override
+	public String nodeName() {
+		return "#document";
+	}
 
-    @Override
-    public String nodeValue() {
-        return null;
-    }
+	@Override
+	public String nodeValue() {
+		return null;
+	}
 
-    @Override
-    public short nodeType() {
-        return DOCUMENT_NODE;
-    }
+	@Override
+	public short nodeType() {
+		return DOCUMENT_NODE;
+	}
 
-    @Override
-    public Node parentNode() {
-        return null;
-    }
+	@Override
+	public Node parentNode() {
+		return null;
+	}
 
-    @Override
-    public NodeList childNodes() {
+	@Override
+	public NodeList childNodes() {
 		NodeList list = new NodeList();
 		Node child = documentElement();
 		if(child != null)
 			list.add(child);
-        return list;
-    }
-    
-    @Override
-    public Node firstChild() {
-        return documentElement();
-    }
+		return list;
+	}
+	
+	@Override
+	public Node firstChild() {
+		return documentElement();
+	}
 
-    @Override
-    public Node lastChild() {
-        return documentElement();
-    }
+	@Override
+	public Node lastChild() {
+		return documentElement();
+	}
 
-    @Override
-    public Node previousSibling() {
-        return null;
-    }
+	@Override
+	public Node previousSibling() {
+		return null;
+	}
 
-    @Override
-    public Node nextSibling() {
-        return null;
-    }
+	@Override
+	public Node nextSibling() {
+		return null;
+	}
 
-    @Override
-    public NamedNodeMap attributes() {
-        return null;
-    }
+	@Override
+	public NamedNodeMap attributes() {
+		return null;
+	}
 
-    @Override
-    public Document ownerDocument() {
-        return null;
-    }
+	@Override
+	public Document ownerDocument() {
+		return null;
+	}
 
-    @Override
-    public Node appendChild(Node newChild) throws DOMException {
-    	if(newChild != null){
+	@Override
+	public Node appendChild(Node newChild) throws DOMException {
+		if(newChild != null){
 			setDocumentElement(newChild);
 		}
 		else{
 			throw new NullPointerException("newChild is null");
 		}
-        return newChild;
-    }
+		return newChild;
+	}
 
-   @Override
-    public Node insertBefore(Node newChild, Node refChild) throws DOMException {
+	@Override
+	public Node insertBefore(Node newChild, Node refChild) throws DOMException {
 		if(refChild == null){
 			appendChild(newChild);
 		}
@@ -193,11 +193,11 @@ public class Document extends JFrame implements Node{
 				}
 			}
 		}
-        return newChild;
-    }
+		return newChild;
+	}
 
-    @Override
-    public Node replaceChild(Node newChild, Node oldChild) throws DOMException {
+	@Override
+	public Node replaceChild(Node newChild, Node oldChild) throws DOMException {
 		if(oldChild != documentElement()){
 			throw new DOMException(DOMException.NOT_FOUND_ERR, "oldChild is not found");
 		}
@@ -207,56 +207,56 @@ public class Document extends JFrame implements Node{
 		
 		setDocumentElement(newChild);
 		return newChild;
-    }
+	}
 
-    @Override
-    public Node removeChild(Node oldChild) throws DOMException {
-    	if(oldChild != documentElement()){
-    		throw new DOMException(DOMException.NOT_FOUND_ERR, "oldChild is not found");
+	@Override
+	public Node removeChild(Node oldChild) throws DOMException {
+		if(oldChild != documentElement()){
+			throw new DOMException(DOMException.NOT_FOUND_ERR, "oldChild is not found");
 		}
 		setDocumentElement((Element)null);
 		
-        return oldChild;
-    }
+		return oldChild;
+	}
 
-    @Override
-    public boolean hasChildNodes() {
-        return documentElement() != null;
-    }
+	@Override
+	public boolean hasChildNodes() {
+		return documentElement() != null;
+	}
 
-    @Override
-    public boolean hasAttributes() {
-        return false;
-    }
+	@Override
+	public boolean hasAttributes() {
+		return false;
+	}
 
-    @Override
-    public void addEventListener(String type, EventListener listener,
-            boolean useCapture) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void addEventListener(String type, EventListener listener,
+			boolean useCapture) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void removeEventListener(String type, EventListener listener,
-            boolean useCapture) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void removeEventListener(String type, EventListener listener,
+			boolean useCapture) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public boolean dispatchEvent(Event evt) throws EventException {
-        // TODO Auto-generated method stub
-        return false;
-    }
-    
-    @Override
-    protected void addImpl(Component comp, Object constraints, int index){
+	@Override
+	public boolean dispatchEvent(Event evt) throws EventException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	protected void addImpl(Component comp, Object constraints, int index){
 		if(documentElement() == null){
 			if(comp instanceof Element){
 				super.addImpl(comp, constraints, index);
 			}
 		}
-    }
+	}
 
 	private Element getElementByIdInternal(Element parent, String elementId){
 		if(parent != null){
