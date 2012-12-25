d7f270e891f0fce46e9b06c2645249e7d31fcbbf
southernbear <xq5530303@gmail.com>
2012-12-25 23:43:32 +0800

    Test attribute methods

--- test/html2windows/dom/ElementTest.java
+++ test/html2windows/dom/ElementTest.java
@@ -14,4 +14,28 @@ public class ElementTest{
 	public void before(){
 		element = document.createElement("div");
 	}
+
+	@Test
+	public void attribute(){
+		HashMap<String, String> attr = new HashMap<String, String>();
+		attr.put("id", "container");
+		attr.put("class", "red-box");
+
+		assertNull("Not existing name should return null", element.getAttribute("id"));
+		assertNull("Not existing name should return null", element.getAttributeNode("id"));
+
+		assertNull("Pass null should return null", element.getAttribute(null));
+
+		for(String name : attr.keySet()){
+			String value = attr.get(name);
+			element.setAttribute(name, value);
+			assertEquals("'" + name + "' should get value '" + value + "'", element.getAttribute(name), value);
+		}
+
+		for(String name : attr.keySet()){
+			String value = attr.get(name);
+			element.removeAttribute(name);
+			assertNull("'" + name + "' should get null", element.getAttribute(name));
+		}
+	}
 }
