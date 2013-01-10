b1b3f2f9e5904b106cb35547399f2f35ba6e3219
bee040811 <bee040811@hotmail.com>
2013-01-10 10:23:04 +0800

    Font property and test

---
+++ src/html2windows/css/FontProperty.java
@@ -0,0 +1,185 @@
+package html2windows.css;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Font;
+import java.awt.font.FontRenderContext;
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.text.AttributedString;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JComponent;
+
+import html2windows.dom.Document;
+import html2windows.dom.Element;
+import html2windows.css.Style;
+
+import javax.swing.text.StyleConstants; 
+
+
+class WindowEventHandler extends WindowAdapter {
+    int count = 0;
+    
+    public WindowEventHandler() {
+        while(this.count == 0){}
+    }
+    public void windowClosing(WindowEvent evt) {
+        this.count = 1;
+    }
+}
+@SuppressWarnings(value= { "serial" })
+public class FontProperty extends JPanel implements CSSPainter {
+
+    private Graphics2D g2d;
+    private Graphics fontd;
+
+    private String text;
+    private Font font;
+    HashMap<String, String> property = new HashMap<String, String>();
+
+    public FontProperty(String input) { 
+        this.text = input;
+        initial();
+    }
+
+    public void initial() {
+        this.property.put("family","Arial");
+    }
+    @Override 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics tmp = g;
+        this.g2d = (Graphics2D) g;
+
+        setFont();
+        //AttributedString fontAttr = setTextDecoration(this.font,this.text);
+        int fontwidth = 100;
+        int textIndent = 0;//= setTextIndent();
+        this.fontd = tmp.create(textIndent,0,fontwidth,400);
+        this.fontd.setFont(this.font);
+        this.fontd.setColor(Color.orange);
+        this.fontd.drawString(this.text, 35, 25);
+        //this.fontd.drawString(fontAttr.getIterator(), 35, 25);
+
+        this.fontd = tmp.create(100,0,300,100);
+        this.fontd.setFont(this.font);
+        this.fontd.setColor(Color.orange);
+        this.fontd.drawString(this.text, 35, 25);
+    }
+    
+    private int _setVariant() {
+        if(this.property.get("font-variant") != null) {
+            String variant = this.property.get("font-variant").toLowerCase();
+            if(variant == "small-caps") {
+                this.text = this.text.toUpperCase();
+                return -5;
+            }
+        }
+        return 10;
+    }
+
+    private int _setFontSize() {
+        int size = 12;
+        int variantSize = _setVariant(); 
+        size = size + variantSize;
+        if(this.property.get("font-size") == null) {
+            return size;
+        }
+        else {
+            return Integer.parseInt(this.property.get("font-size"));         
+        }
+    }
+    private void setFont() {
+        int size = _setFontSize();
+        int weight = _setFontWeight();
+        String family = this.property.get("family");
+
+        this.font = new Font(family, weight, size);
+    }
+    /**
+     *
+     *  
+     *
+     *
+     *
+     */
+     
+
+    private int _setFontWeight() {
+        String strWeight = this.property.get("font-weight").toLowerCase();
+        String strStyle = this.property.get("font-style").toLowerCase();
+        int weight = Font.PLAIN;
+        if(isNumberic(strWeight)) {
+            int tmpWeight = Integer.parseInt(strWeight); 
+        }
+        if(strWeight == "bold" ) {
+            weight = Font.BOLD;       
+        }
+        else if(strWeight == "italic" ) {
+            weight = Font.ITALIC;       
+        }
+        else if(strWeight == "plain") {
+            weight = Font.PLAIN;       
+        }
+        else if(strWeight == "bold+italic") {
+            weight = Font.BOLD + Font.ITALIC;       
+        }
+        if(weight == Font.BOLD && strStyle == "italic") {
+            weight = Font.BOLD + Font.ITALIC;       
+        }
+        return weight;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+        this.repaint();
+    }
+
+    public void paint(Style style, Element element, Graphics g) {
+        this.g2d = (Graphics2D) g;
+        setFontStyle(style,"family");
+        setFontStyle(style,"font-size");
+        setFontStyle(style,"font-weight");
+        setFontStyle(style,"font-style");
+    }
+    public void setFontStyle(Style style,String name) {
+        String type = style.getProperty(name);
+        if( type != null ) {
+            this.property.put(name, type) ;    
+        }
+    };
+    private boolean isNumberic(String name) {
+       Pattern pattern = Pattern.compile("[0-9]*");
+       Matcher isNum = pattern.matcher(name);
+       if(!isNum.matches() ){
+           return false;
+       }
+       return true;
+    }
+    /*
+    public void testPaint(int test1) {
+        switch(test1){
+            case 1:{
+                this.text = "abcc"
+                break; 
+            }
+            case 2: {
+                this.g2d.drawString("avs", 35, 25);
+                break;    
+            }
+        }
+    }
+    */
+
+
+}
---
+++ test/html2windows/css/FontTest.java
@@ -0,0 +1,70 @@
+package html2windows.dom;
+import java.io.File;
+
+import html2windows.css.FontProperty;
+import html2windows.css.Style;
+import html2windows.dom.ElementInter;
+import html2windows.dom.Document;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class FontTest { 
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+    
+    @Test
+    public void test() throws Exception{
+
+        FontProperty font = new FontProperty("family");
+
+        JFrame frame = new JFrame("height");
+        String tagName = "h1";
+        ElementInter fontNode = new ElementInter(tagName);
+        Style fontType = new Style(fontNode);
+        Graphics g = null;
+        fontType.setProperty("font-size","30");
+        String fontSize = fontType.getProperty("font-size");
+        assertEquals("font-size should be 30", "30", fontSize);
+
+        fontType.setProperty("family","Arial");
+        String fontFamily = fontType.getProperty("family");
+        assertEquals("family should be Arial", "Arial", fontFamily );
+
+        fontType.setProperty("font-weight","bold");
+        fontType.setProperty("font-style","italic");
+        fontType.setProperty("font-variant","small-caps");
+
+        font.paint(fontType, fontNode, g);
+        assertEquals("frame should have no window listener", 0, frame.getWindowListeners().length);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(360,200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.add(font);
+        //font.setText("asdf");
+    }
+
+    @Test
+    public void test2(){
+        assert(true);
+        
+    }
+}
