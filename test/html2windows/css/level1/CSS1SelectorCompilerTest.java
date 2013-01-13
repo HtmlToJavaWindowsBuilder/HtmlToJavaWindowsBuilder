95f8a4903e39922e14a20c2d5c36e61fc3b3624c
southernbear <xq5530303@gmail.com>
2013-01-14 01:30:38 +0800

    Split test case

--- test/html2windows/css/level1/CSS1SelectorCompilerTest.java
+++ test/html2windows/css/level1/CSS1SelectorCompilerTest.java
@@ -1,6 +1,7 @@
 package html2windows.css.level1;
 
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 import java.util.HashMap;
@@ -8,39 +9,56 @@ import java.util.HashMap;
 public class CSS1SelectorCompilerTest{
 	CSS1SelectorCompiler compiler;
 	
-	public CSS1SelectorCompilerTest(){
+	@Before
+	public void before(){
 		compiler = new CSS1SelectorCompiler();
+	}
+	
+	@Test
+	public void testNull(){
+		assertNull("Null string should return null", compiler.compile(null));
 	}
 
 	@Test
-	public void compileTest(){
-		
-		assertNull(null, compiler.compile(""));
-		
-		HashMap<String, Class<?>[]> cases = new HashMap<String, Class<?>[]>();
-		
-		cases.put("div", new Class<?>[]{TypeSelector.class});
-		cases.put(".class", new Class<?>[]{ClassSelector.class});
-		cases.put("#id", new Class<?>[]{IDSelector.class});
-		cases.put("div.class", new Class<?>[]{ClassSelector.class, TypeSelector.class});
-		cases.put("div#id.class", new Class<?>[]{ClassSelector.class, IDSelector.class, TypeSelector.class});
-		cases.put("div div", new Class<?>[]{TypeSelector.class, DescendantSelector.class, TypeSelector.class});
-		
-		for (String selectorText : cases.keySet()) {
-			Selector selector = compiler.compile(selectorText);
-			Class<?>[] classes = cases.get(selectorText);
-			int i = classes.length;
-			for (Class<?> klass : classes){
-				assertNotNull("", selector);
-				
-				String errorMessage = 
-					String.format("In case '%s', class of selector %d/%d should be '%s', however got '%s",
-									selectorText, i, classes.length, klass, selector.getClass());
-				assertTrue(errorMessage, klass.isInstance(selector));
-				
-				selector = selector.prev();
-				i--;
-			}
-		}
+	public void testEmpty(){
+		assertNull("Empty string should return null", compiler.compile(""));
+	}
+	
+	@Test
+	public void testTypeSelector(){
+		assertTrue("'div' should return TypeSelector", compiler.compile("div") instanceof TypeSelector);
+	}
+	
+	@Test
+	public void testClassSelector(){
+		assertTrue("'.class' should return ClassSelector", compiler.compile(".class") instanceof ClassSelector);
+	}
+	
+	@Test
+	public void testIDSelector(){
+		assertTrue("'#id' should return IDSelector", compiler.compile("#id") instanceof IDSelector);
+	}
+	
+	@Test
+	public void testTypeAndClassSelector(){
+		Selector selector = compiler.compile("div.class");
+		assertTrue("'.class' should return ClassSelector", selector instanceof ClassSelector);
+		assertTrue("'div' should return TypeSelector", selector.prev() instanceof TypeSelector);
+	}
+	
+	@Test
+	public void testTypeAndIDAndClassSelector(){
+		Selector selector = compiler.compile("div#id.class");
+		assertTrue("'.class' should return ClassSelector", selector instanceof ClassSelector);
+		assertTrue("'#id' should return IDSelector", selector.prev() instanceof IDSelector);
+		assertTrue("'div' should return TypeSelector", selector.prev().prev() instanceof TypeSelector);
+	}
+	
+	@Test
+	public void testDescendantSelector(){
+		Selector selector = compiler.compile("div div");
+		assertTrue("'div' should return TypeSelector", selector instanceof TypeSelector);
+		assertTrue("' ' should return DescendantSelector", selector.prev() instanceof DescendantSelector);
+		assertTrue("'div' should return TypeSelector", selector.prev().prev() instanceof TypeSelector);
 	}
 }
