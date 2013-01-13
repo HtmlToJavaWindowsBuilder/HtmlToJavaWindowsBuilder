4c52a489fa1b3d55b0a2b47e54c432cba9140c9d
southernbear <xq5530303@gmail.com>
2013-01-14 01:11:34 +0800

    Add CSS1 selector compiler test

---
+++ test/html2windows/css/level1/CSS1SelectorCompilerTest.java
@@ -0,0 +1,46 @@
+package html2windows.css.level1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+public class CSS1SelectorCompilerTest{
+	CSS1SelectorCompiler compiler;
+	
+	public CSS1SelectorCompilerTest(){
+		compiler = new CSS1SelectorCompiler();
+	}
+
+	@Test
+	public void testTypeSelector(){
+		
+		assertNull(null, compiler.compile(""));
+		
+		HashMap<String, Class<?>[]> cases = new HashMap<String, Class<?>[]>();
+		
+		cases.put("div", new Class<?>[]{TypeSelector.class});
+		cases.put(".class", new Class<?>[]{ClassSelector.class});
+		cases.put("#id", new Class<?>[]{IDSelector.class});
+		cases.put("div.class", new Class<?>[]{ClassSelector.class, TypeSelector.class});
+		cases.put("div#id.class", new Class<?>[]{ClassSelector.class, IDSelector.class, TypeSelector.class});
+		cases.put("div div", new Class<?>[]{TypeSelector.class, DescendantSelector.class, TypeSelector.class});
+		
+		for (String selectorText : cases.keySet()) {
+			Selector selector = compiler.compile(selectorText);
+			Class<?>[] classes = cases.get(selectorText);
+			int i = classes.length;
+			for (Class<?> klass : classes){
+				assertNotNull("", selector);
+				
+				String errorMessage = 
+					String.format("In case '%s', class of selector %d/%d should be '%s', however got '%s",
+									selectorText, i, classes.length, klass, selector.getClass());
+				assertTrue(errorMessage, klass.isInstance(selector));
+				
+				selector = selector.prev();
+				i--;
+			}
+		}
+	}
+}
