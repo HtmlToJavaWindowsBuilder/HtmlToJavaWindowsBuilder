89d61f0500d126c9678c3ea3bab0f912d5755ab6
southernbear <xq5530303@gmail.com>
2013-01-15 22:22:32 +0800

    Add class selector test

--- test/html2windows/css/level1/CSSSelectorTest.java
+++ test/html2windows/css/level1/CSSSelectorTest.java
@@ -30,4 +30,23 @@ public class CSSSelectorTest{
 		Selector selector = new IDSelector("id");
 		assertTrue("'#id' should match element 'id'", selector.match(element));
 	}
+
+	@Test
+	public void testClassSelector(){
+		Document document = new Document();
+		Element element = document.createElement("div");
+		element.setAttribute("class", "container big");
+
+		Selector selector1 = new ClassSelector("container");
+		assertTrue("'.container' should match element whose class has 'container'", selector1.match(element));
+
+		Selector selector2 = new ClassSelector("big");
+		assertTrue("'.big' should match element whose class has 'big'", selector2.match(element));
+
+		selector1.setPrev(selector2);
+		assertTrue("'.big.container' should match element whose class is 'container big'", selector1.match(element));
+
+
+		
+	}
 }
