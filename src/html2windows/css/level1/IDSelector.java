811568360070b3c05ff16adaed50ecb6c02efb78
southernbear <xq5530303@gmail.com>
2013-01-16 22:04:50 +0800

    Add comment : IDSelector

--- src/html2windows/css/level1/IDSelector.java
+++ src/html2windows/css/level1/IDSelector.java
@@ -3,13 +3,29 @@ package html2windows.css.level1;
 import html2windows.dom.Element;
 import html2windows.dom.Document;
 
+/**
+ * Selector used to check ID.
+ *
+ * @author Southernbear
+ */
 class IDSelector extends SimpleSelector{
+	/**
+	 * ID
+	 */
 	String id;
 
+	/**
+	 * Constructor.
+	 */
 	public IDSelector(String id){
 		this.id = id;
 	}
 
+	/**
+	 * Return whether element's id equals selector's. 
+	 *
+	 * @return Whether element's id equals selector's. 
+	 */
 	protected boolean realMatch(Element element){
 		return id.equals(element.getAttribute("id"));
 	}
