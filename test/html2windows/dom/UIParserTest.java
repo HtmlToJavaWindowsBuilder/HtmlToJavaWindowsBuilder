2b3d21202e7ea368d5514f48666736e4423cde5a
cfwei <s124210@gmail.com>
2013-01-16 21:03:32 +0800

    add comment and test file for UIParserTest

--- test/html2windows/dom/UIParserTest.java
+++ test/html2windows/dom/UIParserTest.java
@@ -9,49 +9,150 @@ import org.junit.Test;
 import html2windows.dom.Document;
 import html2windows.dom.Element;
 
+/**
+ * Test the method in UIParser
+ * 
+ * @author	CFWei
+ *
+ */
+
+
 public class UIParserTest {
-
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-
-    }
+	
+    /**
+     * Test parsing string by UIParser
+     * 
+     */
     @Test
-    public void UIParser() throws Exception{
+    public void testParseString() {
+    	/**	Create a UIParser and string to test
+    	 * 
+    	 */
         UIParser uiParserTest = new UIParser();
-        //String input = "<html><head></head><body>test message</body></html>";
+        String input = "<html><head></head><body>test message</body></html>";
         
-        
-        File input = new File("/home/cfwei-nb/workspace/HtmlToJavaWindowsBuilder/test/html2windows/dom/testfile/test.html");
-        
-        
+        /**Use UIParser to parse string and test test that it is not null.
+         * 
+         * 
+         */
         Document doc = uiParserTest.parse(input);
         assertNotNull(doc);
         
-        
+        /**Use doc to get documentElement and test that it is not null.
+         * 
+         */
         Element html = doc.documentElement();
         assertNotNull(html);
+        
+        /** Test whether the tag Name is correct
+         * 
+         */
         assertEquals("tagName of root element should be <html>", "html", html.tagName());
         
+        /**Get the child list of documentElement and test whether the child count correct 
+         * 
+         */
         NodeList list = html.childNodes();
         assertEquals("list length should be one",list.length(),2);
-
+        
+        /**Get the first child and test whether the node name is correct.
+         * 
+         */
         Node head = list.item(0);
         assertEquals("first child of <html> should be <head>", "head", head.nodeName());
         
+        /**Get the second child and test whether the node name is correct.
+         * 
+         */
         Node body = list.item(1);
         assertEquals("second child of <html> should be <body>", "body", body.nodeName()); 
         
+        /**Get the childlist of second component and test whether the count of list is correct.
+         * 
+         */
         NodeList bodyChildList = body.childNodes();
         assertEquals("list length should be one",bodyChildList.length(),1);
         
+        /**Get the grandchild of second child and test whether the nodeType and node Value are correct. 
+         * 
+         */
         Node bodyChild = bodyChildList.item(0);
         assertEquals("type of first child of <body> should be TEXT_NODE", Text.TEXT_NODE, bodyChild.nodeType());
         assertEquals("value of first child of <employ> should be test message", "test message", bodyChild.nodeValue());
     }
+    
+    
+    
+    
+    /**
+     * Test parsing file by UIParser
+     * 
+     * 
+     */
+    @Test
+    public void testParseFile(){
+    	
+    	
+    	/**	Create a UIParser and set the file path
+    	 * 
+    	 */
+    	UIParser uiParserTest = new UIParser();
+    	File input = new File("test-file/test.html");
+    	
+        /**Use UIParser to parse string and test test that it is not null.
+         * 
+         * 
+         */
+        Document doc = uiParserTest.parse(input);
+        assertNotNull(doc);
+        
+        /**Use doc to get documentElement and test that it is not null.
+         * 
+         */
+        Element html = doc.documentElement();
+        assertNotNull(html);
+        
+        /** Test whether the tag Name is correct
+         * 
+         */
+        assertEquals("tagName of root element should be <html>", "html", html.tagName());
+        
+        /**Get the child list of documentElement and test whether the child count correct 
+         * 
+         */
+        NodeList list = html.childNodes();
+        assertEquals("list length should be one",list.length(),2);
+        
+        /**Get the first child and test whether the node name is correct.
+         * 
+         */
+        Node head = list.item(0);
+        assertEquals("first child of <html> should be <head>", "head", head.nodeName());
+        
+        /**Get the second child and test whether the node name is correct.
+         * 
+         */
+        Node body = list.item(1);
+        assertEquals("second child of <html> should be <body>", "body", body.nodeName()); 
+        
+        /**Get the childlist of second component and test whether the count of list is correct.
+         * 
+         */
+        NodeList bodyChildList = body.childNodes();
+        assertEquals("list length should be one",bodyChildList.length(),1);
+        
+        
+        /**Get the grandchild of second child and test whether the nodeType and node Value are correct. 
+         * 
+         */
+        Node bodyChild = bodyChildList.item(0);
+        assertEquals("type of first child of <body> should be TEXT_NODE", Text.TEXT_NODE, bodyChild.nodeType());
+        assertEquals("value of first child of <employ> should be test message", "test message", bodyChild.nodeValue());
+    }
+    
+    
+   
+    
+    
 }
 
