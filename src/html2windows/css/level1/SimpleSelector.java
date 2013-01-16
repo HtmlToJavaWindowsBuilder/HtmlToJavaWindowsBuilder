6c374803c107518cd1527bbd0abe92c97863a04a
southernbear <xq5530303@gmail.com>
2013-01-16 21:51:38 +0800

    Add comment : SimpleSelector

--- src/html2windows/css/level1/SimpleSelector.java
+++ src/html2windows/css/level1/SimpleSelector.java
@@ -3,7 +3,19 @@ package html2windows.css.level1;
 import html2windows.dom.Element;
 import html2windows.dom.Document;
 
+/**
+ * Template of simple selector
+ *
+ * @author Southernbear
+ */
 abstract class SimpleSelector extends SelectorAdapter{
+	/**
+	 * {@inheritDoc}
+	 *
+	 * Match from last selector to first in the selector chain
+	 * and will be broken and fail once a seletor is not match.
+	 * Call realMatch to match.
+	 */
 	public boolean match(Element element){
 		if (realMatch(element)) {
 			if (prev() == null || (prev() != null && prev().match(element))){
@@ -13,5 +25,11 @@ abstract class SimpleSelector extends SelectorAdapter{
 		return false;
 	}
 	
+	/**
+	 * Selector match function.
+	 *
+	 * @param element Element to be validated.
+	 * @return Whether the element matches the selector.
+	 */
 	protected abstract boolean realMatch(Element element);
 }
