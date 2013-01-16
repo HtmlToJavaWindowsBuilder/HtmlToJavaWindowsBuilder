6da0f0e53b42f36ea380e8b00ce0b875c9c958bd
southernbear <xq5530303@gmail.com>
2013-01-16 21:55:49 +0800

    Add comment : TypeSelector

--- src/html2windows/css/level1/TypeSelector.java
+++ src/html2windows/css/level1/TypeSelector.java
@@ -3,13 +3,29 @@ package html2windows.css.level1;
 import html2windows.dom.Element;
 import html2windows.dom.Document;
 
+/**
+ * Selector for type as known as tag name.
+ *
+ * @author Southernbear
+ */
 class TypeSelector extends SimpleSelector{
+	/**
+	 * Tag name.
+	 */
 	String tagName;
 
+	/**
+	 * Constructor
+	 */
 	public TypeSelector(String tagName){
 		this.tagName = tagName;
 	}
 
+	/**
+	 * Return whether element's tag name equals selector's.
+	 *
+	 * @return Whether element's tag name equals selector's.
+	 */
 	protected boolean realMatch(Element element){
 		return element.nodeName().equals(tagName);
 	}
