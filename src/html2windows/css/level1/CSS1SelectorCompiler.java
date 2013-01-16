4a93e010319a1776e66f4dd9943bfd5494850649
southernbear <xq5530303@gmail.com>
2013-01-16 20:51:14 +0800

    Add comment : CSS1SelectorCompiler

--- src/html2windows/css/level1/CSS1SelectorCompiler.java
+++ src/html2windows/css/level1/CSS1SelectorCompiler.java
@@ -2,12 +2,42 @@ package html2windows.css.level1;
 
 import java.lang.Character;
 
+/**
+ * Compiler for CSS selector level 1.
+ * 
+ * The compile css selector string to a chain of class Selector 
+ * in reverse order.
+ * For example :
+ * 	A string 
+ * 		'#some-node .some-class sometype' 
+ * 	will be compiled to 
+ * 		sometype -> ' ' -> .some-class -> ' ' -> #some-node
+ *
+ * @author Southernbear
+ */
 class CSS1SelectorCompiler{
+	/**
+	 * The last selector in string and the head of the return chain.
+	 */
 	Selector lastSelector;
 
+	/**
+	 * Handled selector string.
+	 */
 	String selector;
+
+	/**
+	 * Current processing position of selector string.
+	 */
 	int position;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param selector Selector string to be compiled.
+	 * @return A Chain of class Selector in reverse order from selector
+	 *         string.
+	 */
 	public Selector compile(String selector){
 		if (selector == null) {
 			return null;
@@ -49,11 +79,22 @@ class CSS1SelectorCompiler{
 		return lastSelector;
 	}
 
+	/**
+	 * Append a selector to chain.
+	 *
+	 * @param selector Selector to be pushed.
+	 */
 	private void push(Selector selector){
 		selector.setPrev(lastSelector);
 		lastSelector = selector;
 	}
-
+	
+	/**
+	 * Extract a identity string from selector string at current position 
+	 * and set position and set the position right behind the identify string.
+	 *
+	 * @return A identity extracted from selector string at current position.
+	 */
 	private String getIdent(){
 		String ident = "";
 		for (int ch = getChar(); isIdentChar(ch); ch = getChar()){
@@ -63,6 +104,12 @@ class CSS1SelectorCompiler{
 		return ident;
 	}
 	
+	/**
+	 * Check whether is a leading character of some recognizable token.
+	 *
+	 * @param ch Character to be cheched.
+	 * @return Whether ch is a leading character of some recoginzable token.
+	 */
 	private boolean isKeyChar(int ch){
 		if (ch == '*' || ch == '#' || ch == '.' || isIdentChar(ch))
 			return true;
@@ -70,6 +117,12 @@ class CSS1SelectorCompiler{
 			return false;
 	}
 
+	/**
+	 * Check whether is a valid character in a identity string.
+	 *
+	 * @param ch  Character to be checkd.
+	 * @return Whether ch is a valid character in a identity string.
+	 */
 	private boolean isIdentChar(int ch){
 		if (ch == '_' || ch == '-' ||  (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')) {
 			return true;
@@ -78,6 +131,12 @@ class CSS1SelectorCompiler{
 			return false;
 	}
 
+	/**
+	 * Get character from selector string at current position and move 
+	 * to next position.
+	 *
+	 * @return Get character from selector string at current position. 
+	 */
 	private int getChar(){
 		try{
 			int ch = selector.charAt(position++);
@@ -88,10 +147,19 @@ class CSS1SelectorCompiler{
 		}
 	}
 	
+	/**
+	 * Move current position back.
+	 */
 	private void backChar(){
 		position--;
 	}
 
+	/**
+	 * Convert integer code to single character string.
+	 *
+	 * @param ch Integer of character code.
+	 * @return Single character coorsponding to ch.
+	 */
 	private String toChar(int ch){
 		return Character.toString((char) ch);
 	}
